79e6a6e1ba4b1548074499319ac4e15f5e5bdd0d
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- XoopsModules/sexy/releases/1.34/htdocs/modules/sexy/language/english/blocks.php
+++ XoopsModules/sexy/releases/1.34/htdocs/modules/sexy/language/english/blocks.php
@@ -1,7 +1,7 @@
 <?php
 
-	define('_BL_SXY_LIMIT', 'Number of up and coming shows to show:');
-	define('_BL_SXY_COLUMNS', 'Number of columns to show:');
-	define('_BL_SXY_ITEM_CAT', 'SHOW');
-	define('_BL_SXY_PREBOOK', 'Prebook');
+	define('_BL_SXY_LIMIT','Number of up and coming shows to show:');
+	define('_BL_SXY_COLUMNS','Number of columns to show:');
+	define('_BL_SXY_ITEM_CAT','SHOW');
+	define('_BL_SXY_PREBOOK','Prebook');
 ?>
